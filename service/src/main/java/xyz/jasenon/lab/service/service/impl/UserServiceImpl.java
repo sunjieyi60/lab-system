7c4932bc866f6d5e9f0b0f7be325d877d8c67f7f
XiaoQ <16121645+ou-xiaoqin@user.noreply.gitee.com>
2026-04-07 21:39:11 +0800

    /laboratory/editManagers  /laboratory/detail这两个接口一下会返回多条数据导致的报错

--- service/src/main/java/xyz/jasenon/lab/service/service/impl/UserServiceImpl.java
+++ service/src/main/java/xyz/jasenon/lab/service/service/impl/UserServiceImpl.java
@@ -144,7 +144,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             if (!labOver){
                 return R.fail("实验室越权");
             }
-            for (Long laboratoryId : laboratoryIds){
+            for (Long laboratoryId : laboratoryIds.stream().distinct().toList()) {
                 LaboratoryUser laboratoryUser = new LaboratoryUser()
                         .setUserId(user.getId())
                         .setLaboratoryId(laboratoryId);
@@ -238,7 +238,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             if (!labOver){
                 return R.fail("实验室越权");
             }
-            for (Long laboratoryId : laboratoryIds){
+            for (Long laboratoryId : laboratoryIds.stream().distinct().toList()) {
                 LaboratoryUser laboratoryUser = new LaboratoryUser()
                         .setUserId(user.getId())
                         .setLaboratoryId(laboratoryId);
